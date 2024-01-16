1e021b24bd2e85a9ffe103a654298e13c692a3b9
buchenkov.v <buchenkov.v@wb.ru>
2024-01-16 19:42:35 +0300

    keycloak backurl key fix 2

--- interceptors/keycloak/interceptor.go
+++ interceptors/keycloak/interceptor.go
@@ -175,13 +175,14 @@ func (i *Interceptor) getParams(md metadata.MD) (params *getTokenParams) {
 func (i *Interceptor) returnRedirectJSON(ctx context.Context, md metadata.MD, providedBackURL string) (*proto.RedirectResponse, error) {
 	statusError := status.New(codes.Unauthenticated, "redirect to keycloak")
 	if providedBackURL != "" {
-		metadata.AppendToOutgoingContext(ctx, "x-http-status-code", strconv.Itoa(http.StatusTemporaryRedirect))
+		grpc.SendHeader(ctx, metadata.Pairs("x-http-status-code", strconv.Itoa(http.StatusTemporaryRedirect)))
+
 		st, _ := status.New(codes.Unauthenticated, "redirect to back_url").WithDetails(&proto.RedirectResponse{
 			RedirectUrl: providedBackURL,
 			Cookies:     md.Get("Set-Cookie")})
 		return nil, st.Err()
 	}
-	metadata.AppendToOutgoingContext(ctx, "x-http-status-code", strconv.Itoa(http.StatusUnauthorized))
+	grpc.SendHeader(ctx, metadata.Pairs("x-http-status-code", strconv.Itoa(http.StatusUnauthorized)))
 	backURL := i.getRedirectURI(md) // Assuming getRedirectURI is adapted for gRPC
 	u := i.keycloakService.GenerateAuthLink(backURL)
 	st, _ := statusError.WithDetails(&proto.RedirectResponse{RedirectUrl: u}, &proto.RedirectResponse{})
