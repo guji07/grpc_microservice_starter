64db4dcdb7a7b99e1481cc53741a05431b6c6f41
buchenkov.v <buchenkov.v@wb.ru>
2024-08-08 11:02:06 +0300

    case fix

--- interceptors/keycloak/service.go
+++ interceptors/keycloak/service.go
@@ -53,7 +53,7 @@ const (
 	ParamName_Host                = "host"
 	ParamName_XOriginalRequestURI = "x-original-request-uri"
 
-	ParamName_FinalBackUrl = "finalBackUrl"
+	ParamName_FinalBackUrl = "finalbackurl"
 
 	//already proxying by default grpc-gateway behaviour:
 	ParamName_RequestURI = "requesturi"
